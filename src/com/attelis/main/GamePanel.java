ef13b2dfef248909d95551e71853c24a6499abf4
Felipe Guimarães <54296382+felipe2g@users.noreply.github.com>
2022-07-31 22:56:42 -0300

    Add screen config to game

---
+++ src/com/attelis/main/GamePanel.java
@@ -0,0 +1,22 @@
+package com.attelis.main;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GamePanel extends JPanel {
+    // SCREEN SETTINGS
+    final int originalTileSize = 16; //16x16 tile
+    final int scale = 3;
+
+    final int tileSize = originalTileSize * scale; // 48x48 tile
+    final int maxScreenCol = 16;
+    final int maxScreenRow = 12;
+    final int screenWidth = tileSize * maxScreenCol; // 768px
+    final int screenHeight = tileSize * maxScreenRow; // 576px
+
+    public GamePanel() {
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+        this.setBackground(Color.BLACK);
+        this.setDoubleBuffered(true);
+    }
+}
